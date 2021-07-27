8fc89c719c340713f632885079f659594e98afd0
moshtab <mdmohsin41439@gmail.com>
2021-07-27 07:13:59 +0530

    checking lengths of lines are equal or greater

--- LengthComparison.java
+++ LengthComparison.java
@@ -6,6 +6,25 @@ public class LengthComparison {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Welcome to Line computation program");
+		
+		System.out.println("enter the end points of line1 : ");
+		double line1 = LengthCal();
+		
+		System.out.println("enter the end points of line2 : ");
+		double line2 = LengthCal();
+		
+		if (line1==line2)
+			System.out.println("Lines are equal");
+		else if (line1>line2)
+			System.out.println("line1 is greater");
+		else
+			System.out.println("line2 is greater");
+		
+	}
+
+	private static double LengthCal() {
+		
 		Scanner s = new Scanner(System.in);
 		System.out.println("enter the value of x1 : ");
 		double x1 = s.nextDouble();
@@ -21,7 +40,7 @@ public class LengthComparison {
 		
 		double length = Math.sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
 		System.out.println("length of a line is : " +length);
-		
+		return length;
 	}
 
 }
