564ea0e787fc38deb55538374edda27356801374
moshtab <mdmohsin41439@gmail.com>
2021-07-27 06:28:37 +0530

    calculating length of a line

--- LengthComparison.java
+++ LengthComparison.java
@@ -1,10 +1,27 @@
 package lengthcom;
 
+import java.util.Scanner;
+
 public class LengthComparison {
 
 	public static void main(String[] args) {
 		
-	System.out.println("Welcome to Length Comparison Program");	
+		Scanner s = new Scanner(System.in);
+		System.out.println("enter the value of x1 : ");
+		double x1 = s.nextDouble();
+		
+		System.out.println("enter the value of y1");
+		double y1 = s.nextDouble();
+		
+		System.out.println("enter the value of x2");
+		double x2 = s.nextDouble();
+		
+		System.out.println("enter the value of y2");
+		double y2 = s.nextDouble();
+		
+		double length = Math.sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+		System.out.println("length of a line is : " +length);
+		
 	}
 
 }
